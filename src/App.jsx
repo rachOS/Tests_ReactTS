3a92811b52789cc65e5b35efa3162d55b7e1c5cb
rachOS <g.chamekh@e-nautia.com>
2021-06-20 13:18:24 +0200

    [FEATURE] implement class Point to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
 import { hot } from 'react-hot-loader';
-import test from './Test.ts';
 import { Point } from './core/entities/Point';
+import React from 'react';
 
 function App() {
-  const points = new Point(8, 5);
-  console.log('Point', points.Compare(1));
-
-  return <p> {test}</p>;
+  const p1 = new Point(8, 5);
+  const p2 = new Point(12, 5);
+  return (
+    <>
+      <ul>
+        {' '}
+        Point 1<li>{`x: ${p1.x}`}</li>
+        <li>{`y: ${p1.y}`}</li> Point 2<li>{`x: ${p2.x}`}</li>
+        <li>{`y: ${p2.y}`}</li>
+        <p>{`Compare Point1 with Point2 -> ${p1.Compare(p2)}`}</p>
+      </ul>
+    </>
+  );
 }
 
 export default hot(module)(App);
